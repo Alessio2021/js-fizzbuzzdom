fce3b78fcb75275b56e9c1606b2b8269c815ceb8
Alessio2021 <alessiobut1911@gmail.com>
2021-11-23 15:09:03 +0100

    punteggiatura fixed

--- js/script.js
+++ js/script.js
@@ -1,28 +1,27 @@
 const container = document.querySelector(".container");
 const list = document.querySelector(".list");
-const div = document.createElement('div');
 
 // creo 100 numeri e li stampo
 
 for (let i = 1; i <= 100; i++) {
     const li = document.createElement('li');
     li.classList.add ("box", "item");
-    list.append(li)
-    li.append(i)
+    list.append(li);
+    li.append(i);
     
     if (i % 3 == 0 && i % 5 == 0) {
-        li.classList.add ('violet')
-        li.innerHTML = 'FizzBuzz'
+        li.classList.add ('violet');
+        li.innerHTML = 'FizzBuzz';
         // fizzbuz
     } else if (i % 3 == 0){
-        li.classList.add ('green')
-        li.innerHTML = 'Fizz'
+        li.classList.add ('green');
+        li.innerHTML = 'Fizz';
         // fizz
     } else if (i % 5 == 0){
-        li.classList.add ('yellow')
-        li.innerHTML = 'Buzz'
+        li.classList.add ('yellow');
+        li.innerHTML = 'Buzz';
         // buzz
     } else {
 
-    }
+    };
 };
