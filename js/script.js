975a3a356ce2927b511f713c2abc6f537fdc5300
Alessio2021 <alessiobut1911@gmail.com>
2021-11-23 15:12:22 +0100

    comments

--- js/script.js
+++ js/script.js
@@ -9,18 +9,20 @@ for (let i = 1; i <= 100; i++) {
     list.append(li);
     li.append(i);
     
+    // se multiplo di 3 e 5 appare FIZZBUZZ in VIOLA
     if (i % 3 == 0 && i % 5 == 0) {
         li.classList.add ('violet');
         li.innerHTML = 'FizzBuzz';
-        // fizzbuz
+
+    // se multipo di 3 appare FIZZ in VERDE
     } else if (i % 3 == 0){
         li.classList.add ('green');
         li.innerHTML = 'Fizz';
-        // fizz
+
+    // se multiplo di 5 appare BUZZ in GIALLO
     } else if (i % 5 == 0){
         li.classList.add ('yellow');
         li.innerHTML = 'Buzz';
-        // buzz
     } else {
 
     };
