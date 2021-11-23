3070d15f771a061aea1b6c0796fd2b884fb11af6
Alessio2021 <alessiobut1911@gmail.com>
2021-11-23 15:03:15 +0100

    fizzbuzz

--- js/script.js
+++ js/script.js
@@ -4,8 +4,25 @@ const div = document.createElement('div');
 
 // creo 100 numeri e li stampo
 
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
     const li = document.createElement('li');
-    li.classList.add ("box");
+    li.classList.add ("box", "item");
     list.append(li)
+    li.append(i)
+    
+    if (i % 3 == 0 && i % 5 == 0) {
+        li.classList.add ('violet')
+        li.innerHTML = 'FizzBuzz'
+        // fizzbuz
+    } else if (i % 3 == 0){
+        li.classList.add ('green')
+        li.innerHTML = 'Fizz'
+        // fizz
+    } else if (i % 5 == 0){
+        li.classList.add ('yellow')
+        li.innerHTML = 'Buzz'
+        // buzz
+    } else {
+
+    }
 };
